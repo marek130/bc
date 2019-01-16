13c0180907aa11efefbdd068cfc34659715fc142
marek130 <marekpastierik15@gmail.com>
2019-01-16 21:10:04 +0100

    add script for generating petri net

---
+++ petriNet.py
@@ -0,0 +1,52 @@
+from snakes.nets import *
+import json
+
+graph = PetriNet('First net')
+graph.add_transition(Transition("Attacker"))
+err = 1
+
+def parseFile(data):
+	global graph, err
+	file = data.split("\n")[:-1]
+	for line in file:
+		print "EER"
+		currentNodes = []
+		currentPredictions = []
+		tmpL, tmpR = line.split('==>')
+		tmpL = tmpL.strip()
+		startVertices = tmpL.split(',')
+		for vertex in startVertices:
+			print vertex
+			if not graph.has_place(vertex):
+				graph.add_place(Place(vertex))
+				graph.add_output(vertex, "Attacker", Variable('p'))
+		print "ERRE"
+		endVertices, supp, conf = tmpR.split("#")
+		conf = float(conf.split(" ")[1].rstrip())
+		conf = str(conf).replace(".","")
+		endVertices = endVertices.strip()
+		endVertices = endVertices.split(',')
+		print "P:O:IIO"
+		for vertex in endVertices:
+			if not graph.has_transition(vertex.upper()):
+				graph.add_transition(Transition(vertex.upper(),  Expression('p>0.5')))
+				for ver in startVertices:
+					print ver
+					graph.add_input(ver, vertex.upper(), Variable("Conf" + conf))
+			else:
+				graph.add_transition(Transition(vertex.upper() + str(err),  Expression('p>0.5')))
+				for ver in startVertices:
+					graph.add_input(ver, vertex.upper() + str(err), Variable("Conf" + conf))
+				err += 1
+			if graph.has_place(vertex):
+				graph.add_output(vertex, vertex.upper(), Variable('p'))
+			print "ASDSDSAADSD"
+
+
+
+def init():
+	data = open("data", "r") 
+	a = data.read()
+	parseFile(a)
+
+init()
