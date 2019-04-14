206713e0c7f42fbe866ee6894b6ca5f054b34ebc
Marek <marekpastierik15@gmail.com>
2019-04-14 18:35:17 +0200

    Refactoring code

--- rulesToNeo4j.py
+++ rulesToNeo4j.py
@@ -2,49 +2,47 @@ from py2neo import Graph, Node, Relationship
 import sys
 
 graph = Graph()
-
-nodes    	  = {}
-companies	  = {}
+globalPreconditions = {}
+globalConclusions   = {}
 relationships = []
 
 def parseFile():
-	global nodes, companies, relationships
+	global globalPreconditions, globalConclusions, relationships
 	file = open(sys.argv[1], "r")
 	
+        attacker = Node("Attacker", name="attacker")
 	for line in file:
-		tmpL, tmpR = line.split('==>')
-		tmpL = tmpL.strip()
-		startVertices = tmpL.split(',')
-		mergeNode = Node("Merge", name = "Merge node")
+                currentPreconditions = []
+		preconditions, conclusions = line.split('==>')
+		preconditions = preconditions.strip()
+		startVertices = preconditions.split(',')
+                
+                for vertex in startVertices:
+                        if vertex not in globalPreconditions:
+                                globalPreconditions[vertex] = Node("Precondition", name=vertex)
+                                relationships.append(Relationship(attacker, "_", globalPreconditions[vertex]))
+			currentPreconditions.append(globalPreconditions[vertex])
 
-		for vertex in startVertices:
-			companyOfNode = ".".join(vertex.split(".")[:-2])
-			nameOfNode 	  = vertex.split(".")[-2:-1][0]
-			attempt       = vertex.split(".")[-1]
-			a = "Company_" + companyOfNode.replace(".", "_")
-			if companyOfNode not in companies:
-				nodes[companyOfNode] 	 = {}
-				companies[companyOfNode] = Node("COMPANY", a, name = companyOfNode)
-			if nameOfNode not in nodes[companyOfNode]:
-				nodes[companyOfNode][nameOfNode] = Node("Node", a, name = nameOfNode, company = companyOfNode)
-				relationships.append(Relationship(companies[companyOfNode], a, nodes[companyOfNode][nameOfNode]))
-			relationships.append(Relationship(nodes[companyOfNode][nameOfNode], attempt, mergeNode, whatAttempt = attempt))
-
-		endVertices, supp, conf = tmpR.split("#")
+		endVertices, supp, conf = conclusions.split("#")
 		endVertices = endVertices.strip()
 		endVertices = endVertices.split(',')
 		for vertex in endVertices:
-			companyOfNode = ".".join(vertex.split(".")[:-2])
-			nameOfNode    = vertex.split(".")[-2:-1][0]
-			attempt       = vertex.split(".")[-1]
-			a = "Company_" + companyOfNode.replace(".", "_")
-			if companyOfNode not in companies:
-				nodes[companyOfNode] 	 = {}
-				companies[companyOfNode] = Node("Company", a, name = companyOfNode) 
-			if nameOfNode not in nodes[companyOfNode]:
-				nodes[companyOfNode][nameOfNode] = Node("Node", a, name = nameOfNode, company = companyOfNode)
-				relationships.append(Relationship(companies[companyOfNode], a , nodes[companyOfNode][nameOfNode]))
-			relationships.append(Relationship(mergeNode, attempt , nodes[companyOfNode][nameOfNode], conf = conf, whatAttempt = attempt))
+                        if vertex not in globalConclusions:
+                                globalConclusions[vertex] = []
+                        globalConclusions[vertex].append(Node("Conclusion", name=vertex, support=supp, confidence=conf))
+                        for precondition in currentPreconditions:
+				relationships.append(Relationship(precondition, "_" , globalConclusions[vertex][-1]))
+
+
+
+def addBackwardEdges():
+        global globalPreconditions, globalConclusions, relationships
+        for conclusion in globalConclusions:
+                if conclusion in globalPreconditions:
+                        for vertex in globalConclusions[conclusion]:
+                                relationships.append(Relationship(vertex, "backward", globalPreconditions[conclusion]))
+
+
 
 def createGraph():	
 	for relationship in relationships:
@@ -53,4 +51,5 @@ def createGraph():
 
 graph.delete_all()
 parseFile()
+addBackwardEdges()
 createGraph()
