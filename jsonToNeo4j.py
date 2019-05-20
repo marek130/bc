e8dcc56edecfbb3ca930cace2dc5e7eb1ad36d26
marek130 <marekpastierik15@gmail.com>
2019-05-20 22:44:08 +0200

    Update jsonToNeo4j.py

--- jsonToNeo4j.py
+++ jsonToNeo4j.py
@@ -14,13 +14,13 @@ def parseFile():
 	json_data=open("./jsonData").read()
 	data = json.loads(json_data)
 
-	for node in data["START_NODES"]:
+	for node in data["StartNodes"]:
 		nameForNode = node["Company"] + "_" + node["Name"] + "_" + node["Event"]
 		objectNode =  Node("node", name=nameForNode)
 		nodesList[nameForNode] = objectNode
 		relationships.append(Relationship(ATTACKER, "_", objectNode))
 
-	for node in data["START_NODES"]: # dict [NAME => OBJECT]
+	for node in data["StartNodes"]: # dict [NAME => OBJECT]
 		nameNode = node["Company"] + "_" + node["Name"] + "_" + node["Event"]
 		for predication in node["Predictions"]:
 			nameForPredicate = predication["Company"] + "_" + predication["Name"] + "_" + predication["Event"]
@@ -31,7 +31,7 @@ def parseFile():
 				for key, currentNode in nodesList.items():
 						if key == matchName:
 							relationships.append(Relationship(currentNode, "_", predicatis))
-			for nodis in data["START_NODES"]:
+			for nodis in data["StartNodes"]:
 				reverseNodeName = nodis["Company"] + "_" + nodis["Name"] + "_" + nodis["Event"]
 				if reverseNodeName == nameForPredicate:
 					relationships.append(Relationship(predicatis, "I", nodesList[reverseNodeName]))
